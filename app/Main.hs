f98e94d365a1e0bd0439eda72d3d46cd9167ac36
smudgecat123 <sha.dow@hotmail.co.uk>
2018-07-21 21:02:43 +0100

    got db working

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,13 @@
 module Main where
 
 import Lib
+import Database.HDBC.Sqlite3 (connectSqlite3)
+import Database.HDBC
 
 main :: IO ()
 main = someFunc
+
+
 
 {-
            help menu: stack
@@ -24,4 +28,19 @@ Library packages come from stackage by default in order to prevent dependency he
 
 Always search for library package documentation on stackage website and ensure that
 the version is the same as the once you're using.
+-}
+
+{-
+I installed sqlite3 and the dev library that goes with it into linux with this:
+sudo apt-get install sqlite3 libsqlite3-dev
+
+I then added the following dependencies to my executable "build-depends" in the cabal file:
+HDBC, HDBC-sqlite3
+
+I then added the following dependency to my extra dependencies section in the stack.yaml file:
+extra-deps: [HDBC-sqlite3-2.3.3.1]
+
+I then added the following imports to this main file:
+import Database.HDBC.Sqlite3 (connectSqlite3)
+import Database.HDBC
 -}
