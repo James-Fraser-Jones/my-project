f91b04f73fb0b2c9eb5262625088cfa685dd125a
smudgecat123 <sha.dow@hotmail.co.uk>
2018-07-21 22:38:52 +0100

    got a record in there

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,42 @@
 module Main where
 
 import Lib
+import System.Directory (doesFileExist)
 import Database.HDBC.Sqlite3 (connectSqlite3)
 import Database.HDBC
 
 main :: IO ()
 main = someFunc
 
+{-
+validation for creating a new database:
+1. If string is empty, it's invalid (length == 0)
+2. Check for illegal ch
+2. If string does not contain ".db" file extension, add it ????
+3. If newly modified filename already exists (doesFileExist), it's invalid
+-}
 
+auctionTableName :: String
+auctionTableName = "auction"
+
+createAuctionTable :: IConnection conn => conn -> IO ()
+createAuctionTable = flip runRaw $ "CREATE TABLE " ++ auctionTableName ++ " (Vendor INTEGER, LotNumber INTEGER, Description TEXT, Reserve REAL, PreSaleBids REAL, SalePrice REAL, Purchaser INTEGER, SaleID INTEGER)"
+
+addItem :: IConnection conn => conn -> IO Statement
+addItem = flip prepare $ "INSERT INTO " ++ auctionTableName ++ " VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+
+createDB :: String -> IO ()
+createDB name = do
+  conn <- connectSqlite3 name --connect to the database
+  createAuctionTable conn
+  add <- addItem conn
+  execute add [toSql (5 :: Int), toSql (2 :: Int), toSql "Wooden Chair", toSql (4.50 :: Double), toSql (2.44 :: Double), toSql (10.21 :: Double), toSql (22 :: Int), toSql (1 :: Int)]
+  commit conn --commit changes
+  disconnect conn --good to be explicit about disconnecting at the end
 
 {-
+Stack tutorial:
+
            help menu: stack
             run ghci: stack ghci
 
@@ -31,6 +58,8 @@ the version is the same as the once you're using.
 -}
 
 {-
+HDBC and SQLite3 Set up:
+
 I installed sqlite3 and the dev library that goes with it into linux with this:
 sudo apt-get install sqlite3 libsqlite3-dev
 
